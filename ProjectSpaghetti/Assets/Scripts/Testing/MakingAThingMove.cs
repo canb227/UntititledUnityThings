2a9933fe670e97ef75fe704b8b24b4bd3ba02a15
AnthonyRaiti <tony.raiti@gmail.com>
2018-05-20 18:04:18 -0400

    Babys first project

---
+++ ProjectSpaghetti/Assets/Scripts/Testing/MakingAThingMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakingAThingMove : MonoBehaviour {
+
+	public int upForceMod;
+	Rigidbody rb; //Storing the objects own rigidbody
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector3.up * upForceMod); //Adding upwards force while button is pressed down
+		}
+	}
+}
