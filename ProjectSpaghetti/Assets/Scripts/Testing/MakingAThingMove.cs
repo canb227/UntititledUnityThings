80c6a94d82017aceeec44680aab625174112144e
AnthonyRaiti <tony.raiti@gmail.com>
2018-05-21 17:04:42 -0400

    Folder Update

--- ProjectSpaghetti/Assets/Scripts/Testing/MakingAThingMove.cs
+++ ProjectSpaghetti/Assets/Scripts/Testing/MakingAThingMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MakingAThingMove : MonoBehaviour {
 
-	public int upForceMod;
+	public int upForceMod, forwardForceMod, rotateMod;
 	Rigidbody rb; //Storing the objects own rigidbody
 
 	// Use this for initialization
@@ -14,9 +14,18 @@ public class MakingAThingMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKey(KeyCode.Space))
 		{
-			rb.AddForce(Vector3.up * upForceMod); //Adding upwards force while button is pressed down
+			rb.AddForce(Vector3.up * upForceMod * Time.deltaTime); //Adding upwards force while button is pressed down
 		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.forward * forwardForceMod * Time.deltaTime);
+		}
+		// if(Input.GetKey(KeyCode.R))
+		// {
+		// 	transform.Rotate(new Vector3(0,1,0) * rotateMod * Time.deltaTime);
+		// }
+		transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X"),0) * rotateMod);
 	}
 }
