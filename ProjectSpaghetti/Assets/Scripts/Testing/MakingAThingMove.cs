be8f48977afcdbe461c1f7ffdf5d9fe83194f74c
jeffrey <jeffercizer@gmail.com>
2018-05-24 16:00:35 -0400

    Added a new scene called Camera Test with a FPS and TPS Camera, Also modified the making a thing move script and included lots of public bools to turn on and off stuff if needed

--- ProjectSpaghetti/Assets/Scripts/Testing/MakingAThingMove.cs
+++ ProjectSpaghetti/Assets/Scripts/Testing/MakingAThingMove.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MakingAThingMove : MonoBehaviour {
-
-	public int upForceMod, forwardForceMod, rotateMod;
+	public bool horizontalMouseRotation, verticalMouseRotation, lockZRotation, lockYRotation, lockXRotation, invertVerticalMouseLook;
+	public int upForceMod, forwardForceMod, leftForceMod, rightForceMod, backwardForceMod, horizontalRotateMod, verticalRotateMod;
 	Rigidbody rb; //Storing the objects own rigidbody
 
 	// Use this for initialization
@@ -18,14 +18,81 @@ public class MakingAThingMove : MonoBehaviour {
 		{
 			rb.AddForce(Vector3.up * upForceMod * Time.deltaTime); //Adding upwards force while button is pressed down
 		}
-		if(Input.GetKey(KeyCode.W))
+		if (Input.GetKey (KeyCode.W)) 
 		{
-			transform.Translate(Vector3.forward * forwardForceMod * Time.deltaTime);
+			if (!Input.GetKey (KeyCode.S)) 
+			{
+				transform.Translate (Vector3.forward * forwardForceMod * Time.deltaTime);
+			}
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			if (!Input.GetKey (KeyCode.W)) 
+			{
+				transform.Translate (-Vector3.forward * backwardForceMod * Time.deltaTime);
+			}
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			if (!Input.GetKey (KeyCode.A)) 
+			{
+				transform.Translate (Vector3.right * rightForceMod * Time.deltaTime);
+			}
+		}
+		else if (Input.GetKey (KeyCode.A)) 
+		{
+			if (!Input.GetKey (KeyCode.D)) 
+			{
+				transform.Translate (Vector3.left * leftForceMod * Time.deltaTime);
+			}
 		}
 		// if(Input.GetKey(KeyCode.R))
 		// {
 		// 	transform.Rotate(new Vector3(0,1,0) * rotateMod * Time.deltaTime);
 		// }
-		transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X"),0) * rotateMod);
+		if (horizontalMouseRotation) 
+		{
+			transform.Rotate (new Vector3 (0, Input.GetAxis ("Mouse X"), 0) * horizontalRotateMod * Time.deltaTime);
+		}
+		if (verticalMouseRotation && !invertVerticalMouseLook) {
+			transform.Rotate (new Vector3 (-Input.GetAxis ("Mouse Y"), 0, 0) * verticalRotateMod * Time.deltaTime);
+		} else if(verticalMouseRotation) {
+			transform.Rotate (new Vector3 (Input.GetAxis ("Mouse Y"), 0, 0) * verticalRotateMod * Time.deltaTime);
+		}
+
+		//Code for locking rotation based on booleans
+		if (lockXRotation) {
+			if (lockYRotation) {
+				if (lockZRotation) {
+					transform.eulerAngles = new Vector3 (0, 0, 0);
+				} else {
+					transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+				}	
+			} else {
+				transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, transform.eulerAngles.z);
+			}	
+		}
+		if (lockYRotation) {
+			if (lockXRotation) {
+				if (lockZRotation) {
+					transform.eulerAngles = new Vector3 (0, 0, 0);
+				} else {
+					transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+				}
+			} else {
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 0, transform.eulerAngles.z);
+			}
+		}
+		if (lockZRotation) {
+			if (lockXRotation) {
+				if (lockYRotation) {
+					transform.eulerAngles = new Vector3 (0, 0, 0);
+				} else {
+					transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
+				}
+			} else {
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, 0);
+			}
+		}
 	}
 }
